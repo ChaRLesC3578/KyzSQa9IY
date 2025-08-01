f4ec59e65e44b0e001e7671091164ca44f1e040a
ChaRLesC3578 <waifs_03arcade@icloud.com>
2025-08-01 01:08:51 +0100

    Implement 网络连接状态检查器 using PYQT

---
+++ network_status_checker_0801_0808_ohp.py
@@ -0,0 +1,7 @@
+# 代码生成时间: 2025-08-01 08:08:50
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QMessageBox
+from PyQt5.QtCore import Qt
+import requests
+
+"

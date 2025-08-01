810c85fb30103102476172b284015b0a05f091e9
ChaRLesC3578 <waifs_03arcade@icloud.com>
2025-08-01 12:15:28 +0100

    Introduce new 图片尺寸批量调整器 feature

---
+++ image_resizer_0801_1915_thc.py
@@ -0,0 +1,8 @@
+# 代码生成时间: 2025-08-01 19:15:27
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QFileDialog, QLabel, QSlider, QSpinBox
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap
+from PIL import Image
+
+"

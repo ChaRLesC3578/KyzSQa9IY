fb9b4ce670e4c13f75f89bff74d6c9af673083d6
ChaRLesC3578 <waifs_03arcade@icloud.com>
2025-08-02 18:49:00 +0100

    Add Excel表格自动生成器

---
+++ excel_generator_0803_0148_qqa.py
@@ -0,0 +1,7 @@
+# 代码生成时间: 2025-08-03 01:48:58
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QFileDialog, QVBoxLayout, QMessageBox
+from openpyxl import Workbook, load_workbook
+from openpyxl.utils import get_column_letter
+
+"

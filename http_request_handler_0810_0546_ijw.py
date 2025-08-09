e3583391083da0bfa37fa9d4faf79af5761cac75
ChaRLesC3578 <waifs_03arcade@icloud.com>
2025-08-09 22:46:08 +0100

    Add HTTP请求处理器

---
+++ http_request_handler_0810_0546_ijw.py
@@ -0,0 +1,7 @@
+# 代码生成时间: 2025-08-10 05:46:07
+import sys
+import requests
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QLabel
+from PyQt5.QtCore import QUrl, QCoreApplication
+
+"

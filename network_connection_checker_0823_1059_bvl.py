c133238c1d332d90a2fff2b3cfc80120be8470a9
ChaRLesC3578 <waifs_03arcade@icloud.com>
2025-08-23 03:59:01 +0100

    Add new 网络连接状态检查器 feature

---
+++ network_connection_checker_0823_1059_bvl.py
@@ -0,0 +1,8 @@
+# 代码生成时间: 2025-08-23 10:59:00
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel
+# 增强安全性
+from PyQt5.QtCore import QThread, pyqtSignal
+import requests
+
+"

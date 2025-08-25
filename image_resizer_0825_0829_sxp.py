77a3d9a980736ae27ddce0cbb05c4f72cc82cef8
ChaRLesC3578 <waifs_03arcade@icloud.com>
2025-08-25 01:29:56 +0100

    Add new 图片尺寸批量调整器 feature

---
+++ image_resizer_0825_0829_sxp.py
@@ -0,0 +1,10 @@
+# 代码生成时间: 2025-08-25 08:29:55
+import sys
+import os
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QFileDialog, QLabel, QVBoxLayout, QInputDialog, QLineEdit
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+from PIL import Image
+
+"
+# FIXME: 处理边界情况

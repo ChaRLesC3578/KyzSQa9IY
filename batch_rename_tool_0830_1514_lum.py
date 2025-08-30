7f01a48d4395164ba52fa5fe90e62410bf82da63
ChaRLesC3578 <waifs_03arcade@icloud.com>
2025-08-30 08:14:13 +0100

    Introduce new 批量文件重命名工具 feature

---
+++ batch_rename_tool_0830_1514_lum.py
@@ -0,0 +1,8 @@
+# 代码生成时间: 2025-08-30 15:14:12
+import os
+import re
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QFileDialog, QLineEdit, QLabel
+from PyQt5.QtCore import Qt
+import sys
+
+"

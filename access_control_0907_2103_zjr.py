3f8b9cd85a73fe9edbd8bc55df8fae7d8e2ba2a6
ChaRLesC3578 <waifs_03arcade@icloud.com>
2025-09-07 14:03:01 +0100

    Enhance 访问权限控制 using PYQT

---
+++ access_control_0907_2103_zjr.py
@@ -0,0 +1,7 @@
+# 代码生成时间: 2025-09-07 21:03:00
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QPushButton, QLabel, QLineEdit, QMessageBox
+# 增强安全性
+from PyQt5.QtCore import Qt
+
+"

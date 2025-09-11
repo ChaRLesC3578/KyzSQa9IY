2aaad977b97d387fefce8555c0479a5618754cc5
ChaRLesC3578 <waifs_03arcade@icloud.com>
2025-09-11 11:31:57 +0100

    Setup 文件夹结构整理器

---
+++ folder_structure_organizer_0911_1831_oez.py
@@ -0,0 +1,7 @@
+# 代码生成时间: 2025-09-11 18:31:56
+import os
+import glob
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QFileDialog
+from PyQt5.QtCore import pyqtSlot
+
+"

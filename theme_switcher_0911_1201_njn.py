20c4300a70c5ccc50ed0772c205ae97d64cfa724
ChaRLesC3578 <waifs_03arcade@icloud.com>
2025-09-11 05:01:29 +0100

    Add PYTHON 主题切换功能

---
+++ theme_switcher_0911_1201_njn.py
@@ -0,0 +1,7 @@
+# 代码生成时间: 2025-09-11 12:01:28
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QColorDialog
+from PyQt5.QtGui import QColor
+from PyQt5.QtCore import Qt
+
+"
